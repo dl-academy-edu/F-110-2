95b9218088355dd65b3aaa78d96a3fa8a6e4fcd5
ea-dorosh <directlineacademy@gmail.com>
2021-12-08 14:38:17 +0400

    5

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,16 +1,16 @@
-var bannerBtn = document.querySelector('.banner-button');
+var bannerBtn = document.querySelector('.banner__button');
 var popup = document.querySelector('.popup');
-var closePopup = document.querySelector('.popup-close');
+var closePopup = document.querySelector('.popup__close');
 var input = popup.querySelector('input');
 
 
 bannerBtn.addEventListener('click', function() {
-    popup.classList.add('popup-open');
+    popup.classList.add('popup_open');
     input.focus();
 })
 
 closePopup.addEventListener('click', function() {
-    popup.classList.remove('popup-open');
+    popup.classList.remove('popup_open');
     bannerBtn.focus();
 })
 
@@ -18,7 +18,7 @@ window.addEventListener('keydown',function(event) {
     // console.log(event.code);
     if (event.code === "Escape" && popup.classList.contains('popup-open')) {
         // if(popup.classList.contains('popup-open')) {
-            popup.classList.remove('popup-open');
+            popup.classList.remove('popup_open');
             bannerBtn.focus();
         // }
     }
