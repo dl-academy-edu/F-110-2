a4a0a86ae200553df6bfd0a121d338537fb63aa7
ea-dorosh <directlineacademy@gmail.com>
2021-12-09 11:10:26 +0400

    6

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -16,10 +16,28 @@ closePopup.addEventListener('click', function() {
 
 window.addEventListener('keydown',function(event) {
     // console.log(event.code);
-    if (event.code === "Escape" && popup.classList.contains('popup-open')) {
+    if (event.code === "Escape" && popup.classList.contains('popup_open')) {
         // if(popup.classList.contains('popup-open')) {
             popup.classList.remove('popup_open');
             bannerBtn.focus();
         // }
     }
+})
+
+
+var buttonMobileHeader = document.querySelector('.header__humburger');
+var buttonCloseMenu = document.querySelector('.mobile__close');
+var mobileHeader = document.querySelector('.mobile');
+
+console.log(buttonMobileHeader)
+
+
+buttonMobileHeader.addEventListener('click', function() {
+    mobileHeader.classList.add('mobile__open');
+    console.log("sdfsdfsdf");
+})
+
+buttonCloseMenu.addEventListener('click', function() {
+    mobileHeader.classList.remove('mobile__open');
+    console.log("11");
 })
